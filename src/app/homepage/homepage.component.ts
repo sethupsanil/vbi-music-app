1a9c70316adbee25d8302430fc41586865a64c07
sethu p sanil <sethupsanil666@gmail.com>
2022-08-14 12:50:55 +0530

    preload data added

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -2,6 +2,7 @@ import { SubSink } from 'subsink';
 import { ApiService } from './../core/service/api.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Photo } from '../core/interface/api-interface.interface';
+import { dummyData } from './temp.data';
 
 @Component({
   selector: 'app-homepage',
@@ -10,6 +11,7 @@ import { Photo } from '../core/interface/api-interface.interface';
 })
 export class HomepageComponent implements OnInit, OnDestroy {
   public albumsList: Photo[] = [];
+  public tempData: Photo[] = dummyData;
   private subs = new SubSink();
   constructor(
     private _api: ApiService
